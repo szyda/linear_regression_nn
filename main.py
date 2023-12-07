7e05269a5c02a336d26a508a8c46a3e00ff6ae3e
szyda <szydasylwia@protonmail.com>
2023-12-07 23:02:49 +0100

    remove unused unipolar function

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-def unipolar_function(s):
-    return 1 if s > 0 else 0
-
 def generate_points(number_of_points):
     x = np.linspace(0, 10, number_of_points)
     y = random.uniform(-2, 2) * x + random.uniform(-1, 1) + np.random.randn(number_of_points)
@@ -17,24 +14,24 @@ def generate_weights():
 
 def train_perceptron(x, y, learning_rate=0.01, epochs=50, delta=0.01):
     number_of_points = len(x)
-    a, b = generate_weights()
+    weight_a, weight_b = generate_weights()
 
     for curr_epoch in range(epochs):
-        predicted = a * x + b
+        predicted = weight_a * x + weight_b
         error = y - predicted
 
-        gradient_a = (-2 / number_of_points) * np.dot(error, x)
-        gradient_b = (-2 / number_of_points) * np.sum(error)
+        gradient_descent_a = (-2 / number_of_points) * np.dot(error, x)
+        gradient_descent_b = (-2 / number_of_points) * np.sum(error)
 
-        a -= learning_rate * gradient_a
-        b -= learning_rate * gradient_b
+        weight_a -= learning_rate * gradient_descent_a
+        weight_b -= learning_rate * gradient_descent_b
 
         mean_squared_error = np.mean(error ** 2)
 
         if mean_squared_error < delta:
             break
 
-    return a, b
+    return weight_a, weight_b
 
 def generate_plot(x, y, a, b):
     plt.scatter(x, y, label='Generated points')
