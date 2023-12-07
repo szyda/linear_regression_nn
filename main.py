f71ead657fd6f715c293fa6193a5156bc6ab87a3
szyda <szydasylwia@protonmail.com>
2023-12-07 22:53:19 +0100

    train perceptron

--- main.py
+++ main.py
@@ -14,3 +14,24 @@ def generate_weights():
     weights = np.array([random.uniform(0, 1) for _ in range(2)])
 
     return weights
+
+def train_perceptron(x, y, learning_rate=0.01, epochs=50, delta=0.01):
+    number_of_points = len(x)
+    a, b = generate_weights()
+
+    for curr_epoch in range(epochs):
+        predicted = a * x + b
+        errors = y - predicted
+
+        gradient_a = (-2 / number_of_points) * np.dot(errors, x)
+        gradient_b = (-2 / number_of_points) * np.sum(errors)
+
+        a -= learning_rate * gradient_a
+        b -= learning_rate * gradient_b
+
+        mean_squared_error = np.mean(errors ** 2)
+
+        if mean_squared_error < delta:
+            break
+
+    return a, b
